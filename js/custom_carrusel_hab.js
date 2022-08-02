7f037c45be7107956b558b2eea76e7890be81d71
jampierzcode <jampier127@gmail.com>
2022-08-02 11:02:42 -0500

    mejorando la section habitaciones.html

---
+++ js/custom_carrusel_hab.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.slide-habitaciones', {
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      },
+  });
